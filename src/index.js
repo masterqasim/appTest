9f37225faf0cbb949390cf360f6a960d7c3d23e9
masterqasim <masterqasim95@gmail.com>
2021-05-17 20:43:12 +0500

    reacr-persist

--- src/index.js
+++ src/index.js
@@ -7,12 +7,24 @@ import { createStore ,applyMiddleware} from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './store/reducers/rootReducer'
 import thunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 
-const store=createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store=createStore(persistedReducer, applyMiddleware(thunk));
+let persistor = persistStore(store)
 
 ReactDOM.render(
   <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
   <App />
+  </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
